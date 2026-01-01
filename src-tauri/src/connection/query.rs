7121760dc070ec1f5e97042fe9004b5952e07a70
John Yenter-Briars <jyenterbriars@root16.com>
2026-01-01 10:56:19 -0600

    prove connection

--- src-tauri/src/connection/query.rs
+++ src-tauri/src/connection/query.rs
@@ -2,12 +2,10 @@ use crate::{Database, binding::model::multipleresponse::MultipleResponse, datave
 
 #[tauri::command]
 pub async fn query_results(
-    window: tauri::Window,
+    _window: tauri::Window,
     _number: usize,
     _database: tauri::State<'_, Database>,
 ) -> Result<MultipleResponse, String> {
-    let response_result = reqwest::get("https://www.rust-lang.org").await;
-
     let token = TokenCache::get_token().await?;
 
     let query_engine = QueryEngine::new("https://jyb.crm.dynamics.com/", &token);
@@ -15,18 +13,6 @@ pub async fn query_results(
     let accounts = query_engine.query_accounts(Some("accountnumber eq 'ABSS4G45'")).await?;
 
     println!("Accounts: {}", accounts);
-
-    if let Ok(response) = response_result {
-        if let Ok(body) = response.text().await {
-            println!("body = {body:?}");
-        } else {
-            return Err("error".into());
-        }
-    } else {
-        return Err("error".into());
-    }
-
-    println!("Called from {}", window.label());
 
     let resp = MultipleResponse::new();
 
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -15,8 +15,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .manage(Database {})
-        .invoke_handler(tauri::generate_handler![query_results])
-        .invoke_handler(tauri::generate_handler![create_connection])
+        .invoke_handler(tauri::generate_handler![create_connection, query_results])
         .setup(|app| {
             let windows = app.webview_windows();
             let window = windows.get("QueryVerse").unwrap();
