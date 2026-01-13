000e9d3decdec6050dec1f9219aec594d2322c11
John Yenter-Briars <johnyenterbriars@protonmail.com>
2026-01-13 10:39:01 -0600

    update column width

--- src/components/ResultsWindow.tsx
+++ src/components/ResultsWindow.tsx
@@ -13,6 +13,8 @@ import {
 } from "@fluentui/react-components";
 import { Entity, Value } from "../binding/model/Entity";
 
+const DEFAULT_COL_WIDTH = 300;
+
 function renderValue(value: Value): React.ReactNode {
     if (value === null || value === undefined) return "";
     return String(value);
@@ -33,7 +35,7 @@ export const ResultsWindow = React.memo(({ data }: IResultsWindowProps) => {
                 columnId: attribute,
                 renderHeaderCell: () => attribute,
                 renderCell: (entity) => (
-                    <div style={{ whiteSpace: "normal", wordBreak: "break-word" }}>
+                    <div style={{ whiteSpace: "nowrap" }}>
                         {renderValue(entity.attributes[attribute])}
                     </div>
                 ),
@@ -41,7 +43,7 @@ export const ResultsWindow = React.memo(({ data }: IResultsWindowProps) => {
         );
     }, [data]);
 
-
+    const totalWidth = columns.length * DEFAULT_COL_WIDTH;
 
     return (
         <div
@@ -58,6 +60,7 @@ export const ResultsWindow = React.memo(({ data }: IResultsWindowProps) => {
                 getRowId={
                     (entity: Entity) => String(entity.attributes["accountid"]) ?? ''
                 }
+                style={{ minWidth: totalWidth }}
             >
                 <DataGridHeader
                     style={{
